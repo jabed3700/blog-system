fde7907643139adbde0ed4cbaf0f9d54ba70c075
tamim08 <tamim008bd@gmail.com>
2021-04-12 12:47:43 +0600

    update migration file

--- database/migrations/2021_04_12_052610_create_categories_table.php
+++ database/migrations/2021_04_12_052610_create_categories_table.php
@@ -17,7 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('slug');
-            $table->string('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
